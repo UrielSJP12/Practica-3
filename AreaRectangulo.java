21496a00c2e05b3734d8be504db76780e8ecdbf6
UrielSJP12 <143725665+UrielSJP12@users.noreply.github.com>
2023-09-07 08:46:35 -0600

    Add files via upload

---
+++ AreaRectangulo.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arearectangulo;
+
+/**
+ *
+ * @author labcca
+ */
+public class AreaRectangulo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        int base = 30;
+        int altura = 20;
+        int areaRectangulo = base * altura;
+        
+        System.out.println("Mi nombre es Uriel Jaimez Perez");
+        System.out.println("el area es: " + areaRectangulo);
+    }
+    
+}
